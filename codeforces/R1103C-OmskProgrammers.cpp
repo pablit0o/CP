dd5c7c53f29efc65119b11163365871b570ff7ea
Pablo Silva <pbsilva2009@gmail.com>
2026-06-13 03:41:27 -0700

    Add files via upload

---
+++ codeforces/R1103C-OmskProgrammers.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        ll a, b, x;
+        cin >> a >> b >> x;
+
+        vector<pair<ll, ll>> A, B;
+
+        ll cur = a; 
+        ll d = 0;
+
+        while(true) {
+            A.push_back({cur, d});
+            if(cur == 0){break;}
+            cur /= x;
+            d++;
+        }
+        
+        cur = b;
+        d = 0;
+
+        while (true) {
+            B.push_back({cur, d});
+            if(cur == 0) {break;}
+            cur /= x;
+            d++;
+        }
+
+        ll ans = 10000000000000;
+
+        for (auto [u, da] : A) {
+            for (auto [w, db] : B) {
+                ll v = max(u, w);
+                ll diff =
+                    da + db +
+                    (v - u) +
+                    (v - w);
+                ans = min(ans, diff);
+            }
+        }
+
+        cout << ans << endl;
+    }
+}
