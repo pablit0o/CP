02b33363c850ddde2b9ba878f85c80557eb9938c
Pablo Silva <pbsilva2009@gmail.com>
2026-06-15 01:50:42 -0700

    Add files via upload

---
+++ misc/YS-AssociativeArray.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef long long ll;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef double ld;
+#define all(x) (x).begin(), (x).end()
+
+template<typename T1, typename T2>
+istream& operator>>(istream& in, pair<T1,T2>& p) {
+    return in >> p.first >> p.second;
+}
+
+template<typename T>
+istream& operator>>(istream& in, vector<T>& v) {
+    for (auto &x : v)
+        in >> x;
+    return in;
+}
+
+template<typename T1, typename T2>
+ostream& operator<<(ostream& out, const pair<T1,T2>& p) {
+    return out << p.first << ' ' << p.second;
+}
+
+template<typename T>
+ostream& operator<<(ostream& out, const vector<T>& v) {
+    for (const auto& x : v)
+        out << x << ' ';
+    return out;
+}
+
+int main() {
+    ll n; cin >> n;
+    map<ll, ll> a;
+    for(ll i=0;i<n;i++) {
+        ll q, k; cin >> q >> k;
+        switch(q) {
+            case 0:
+                ll v; cin >> v;
+                a[k] = v;
+                break;
+            case 1:
+                cout << a[k] << endl;
+                break;
+        }
+    }
+}
