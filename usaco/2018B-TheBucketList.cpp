7913d0f05a59e79574141ecc314504cddbc00b19
Pablo Silva <pbsilva2009@gmail.com>
2026-06-15 03:32:20 -0700

    Add files via upload

---
+++ usaco/2018B-TheBucketList.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef long long ll;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef double ld;
+#define all(x) (x).begin(), (x).end()
+
+template<typename T1, typename T2>
+istream& operator>>(istream& in, pair<T1,T2>& p) {
+    return in >> p.first >> p.second;
+}
+
+template<typename T>
+istream& operator>>(istream& in, vector<T>& v) {
+    for (auto &x : v)
+        in >> x;
+    return in;
+}
+
+template<typename T1, typename T2>
+ostream& operator<<(ostream& out, const pair<T1,T2>& p) {
+    return out << p.first << ' ' << p.second;
+}
+
+template<typename T>
+ostream& operator<<(ostream& out, const vector<T>& v) {
+    for (const auto& x : v)
+        out << x << ' ';
+    return out;
+}
+
+bool check(int x, int y, ii posA, ii posB) {
+    if(!(x > posA.first && x <= posB.first)) {return false;}
+    if(!(y > posA.second && y <= posB.second)) {return false;}
+    return true;
+}
+
+int main() {
+    freopen("blist.in","r",stdin);
+    freopen("blist.out","w",stdout);
+    int n; cin >> n;
+    vi s(n), t(n), b(n), ans(1000, 0);
+    for(int i=0;i<n;i++) {
+        cin >> s[i] >> t[i] >> b[i];
+    }
+    for(int i=0;i<n;i++) {
+        for(int j=s[i];j<=t[i];j++) {
+            ans[j] += b[i];
+        }
+    }
+    cout << *max_element(all(ans));
+}
