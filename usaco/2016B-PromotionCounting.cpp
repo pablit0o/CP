d939caf9872b721ea747af130453d6293b11667f
Pablo Silva <pbsilva2009@gmail.com>
2026-06-15 05:07:40 -0700

    Add files via upload

---
+++ usaco/2016B-PromotionCounting.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef long long ll;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef double ld;
+#define all(x) (x).begin(), (x).end()
+
+template<typename T1, typename T2>
+istream& operator>>(istream& in, pair<T1,T2>& p) {
+    return in >> p.first >> p.second;
+}
+
+template<typename T>
+istream& operator>>(istream& in, vector<T>& v) {
+    for (auto &x : v)
+        in >> x;
+    return in;
+}
+
+template<typename T1, typename T2>
+ostream& operator<<(ostream& out, const pair<T1,T2>& p) {
+    return out << p.first << ' ' << p.second;
+}
+
+template<typename T>
+ostream& operator<<(ostream& out, const vector<T>& v) {
+    for (const auto& x : v)
+        out << x << ' ';
+    return out;
+}
+
+int main() {
+    freopen("promote.in","r",stdin);
+    freopen("promote.out","w",stdout);
+    vii inp(4); cin >> inp;
+    int s, g, p;
+    p = inp[3].second - inp[3].first;
+    g = (p+inp[2].second) - inp[2].first;
+    s = (g+inp[1].second) - inp[1].first;
+    cout << s << endl << g << endl << p;
+}
