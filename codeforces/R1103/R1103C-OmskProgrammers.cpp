e391af5d4d6b7d8150dccb7946ab5d8fb9a9893d
Pablo Silva <pablo.silvalizarraga@gmail.com>
2026-06-17 19:25:33 -0700

    Changed rejected code to empty

--- codeforces/R1103/R1103C-OmskProgrammers.cpp
+++ codeforces/R1103/R1103C-OmskProgrammers.cpp
@@ -3,48 +3,33 @@ using namespace std;
 typedef long long ll;
 
 int main() {
-    int t;
-    cin >> t;
-
+    int t; cin >> t;
     while (t--) {
-        ll a, b, x;
-        cin >> a >> b >> x;
-
+        ll a, b, x; cin >> a >> b >> x;
         vector<pair<ll, ll>> A, B;
-
-        ll cur = a; 
-        ll d = 0;
-
+        ll cur = a, d = 0;
         while(true) {
             A.push_back({cur, d});
             if(cur == 0){break;}
             cur /= x;
             d++;
         }
-        
         cur = b;
         d = 0;
-
         while (true) {
             B.push_back({cur, d});
             if(cur == 0) {break;}
             cur /= x;
             d++;
         }
-
         ll ans = 10000000000000;
-
-        for (auto [u, da] : A) {
-            for (auto [w, db] : B) {
+        for(auto [u, da] : A) {
+            for(auto [w, db] : B) {
                 ll v = max(u, w);
-                ll diff =
-                    da + db +
-                    (v - u) +
-                    (v - w);
+                ll diff = da + db + (v - u) + (v - w);
                 ans = min(ans, diff);
             }
         }
-
         cout << ans << endl;
     }
 }
--- codeforces/R1103/R1103G-CriterionInBurlandia.cpp
+++ codeforces/R1103/R1103G-CriterionInBurlandia.cpp
@@ -1,58 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-static const int MAXA = 500000;
-static const int MOD = 1e9 + 7;
-
-int spf[MAXA + 1];
-
-void build_spf() {
-    for (int i = 2; i <= MAXA; i++) {
-        if (!spf[i]) {
-            for (int j = i; j <= MAXA; j += i)
-                if (!spf[j]) spf[j] = i;
-        }
-    }
-}
-
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    build_spf();
-
-    int t;
-    cin >> t;
-
-    while (t--) {
-        int n, x;
-        cin >> n >> x;
-
-        vector<int> a(n);
-        for (auto &v : a) cin >> v;
-
-        unordered_map<int,int> cnt;
-
-        for (int v : a) {
-            unordered_set<int> used_primes;
-
-            while (v > 1) {
-                int p = spf[v];
-                used_primes.insert(p);
-                while (v % p == 0) v /= p;
-            }
-
-            for (int p : used_primes)
-                cnt[p]++;
-        }
-
-        long long ans = 1;
-        for (auto &it : cnt) {
-            ans = ans * it.second % MOD;
-        }
-
-        cout << ans << "\n";
-    }
-
-    return 0;
+    
 }
