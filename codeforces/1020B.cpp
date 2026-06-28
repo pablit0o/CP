a75e89654ed4e8a9f8b5e0b396eacfe7012eaa27
Pablo Silva <pablo.silvalizarraga@gmail.com>
2026-06-28 03:33:11 -0700

    1020B - 06-26-2026

---
+++ codeforces/1020B.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef long long ll;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef double ld;
+typedef vector<bool> vb;
+typedef vector<vb> vvb;
+typedef vector<string> vs;
+typedef vector<vs> vvs;
+#define all(x) (x).begin(), (x).end()
+#define input(x) 
+
+struct Edge
+{
+    int to, weight;
+    Edge(int dest, int w):
+    to(dest), weight(w)
+    {
+    }
+};
+
+// edge list
+// struct Edge{
+//     int a, b, w;
+//     Edge(int start, int end, int weight):
+//     a(start), b(end), w(weight)
+//     {
+//     }
+//     bool operator<(const Edge & e)
+//     const{ 
+//     return w < e.w;
+//     }
+// };
+
+template<typename T1, typename T2>
+istream& operator>>(istream& in, pair<T1,T2>& p) {
+    return in >> p.first >> p.second;
+}
+
+template<typename T>
+istream& operator>>(istream& in, vector<T>& v) {
+    for (auto &x : v)
+        in >> x;
+    return in;
+}
+
+template<typename T1, typename T2>
+ostream& operator<<(ostream& out, const pair<T1,T2>& p) {
+    return out << p.first << ' ' << p.second;
+}
+
+template<typename T>
+ostream& operator<<(ostream& out, const vector<T>& v) {
+    for (const auto& x : v)
+        out << x << ' ';
+    return out;
+
+}
+
+void setIO(string s = "") {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    if (!s.empty()) {
+        freopen((s + ".in").c_str(), "r", stdin);
+        freopen((s + ".out").c_str(), "w", stdout);
+    }
+}
+
+int main() {
+    int n; cin >> n;
+    vii gr(n);
+    for(int i=0;i<n;i++) {
+        int a; cin >> a;
+        gr[i] = make_pair(i, a-1);
+    }
+    // cout << gr << endl;
+    // keep track -> dfs?
+
+    for(int i=0;i<n;i++) {
+        set<int> seen;
+        int ct=0, curr=i;
+        while(true) {
+            // cout << curr << " " << gr[curr].second << " ";
+            seen.insert(curr); ct++;
+            if(seen.size()!=ct) {cout << curr+1 << " "; break;}
+            curr = gr[curr].second;
+        }
+        // cout << endl;
+    }
+}
